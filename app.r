0e0c5b54cfa53b6872ee5c94c61b027213fc643a
willemsleegers <w.sleegers@me.com>
2022-08-31 01:19:08 +0200

    Update app.r

--- app.r
+++ app.r
@@ -5,4 +5,4 @@ if (port == "") port <- "8000"
 
 root <- pr("plumber.r")
 
-pr_run(root, host = "0.0.0.0", port = as.numeric(port))
+pr_run(root, host = "0.0.0.0", port = as.numeric(port), docs = "swagger")
