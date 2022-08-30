8e9c1246c8688d37e76fce5c79c5d85c788ee152
willemsleegers <w.sleegers@me.com>
2022-08-30 23:43:26 +0200

    Update app.r

--- app.r
+++ app.r
@@ -3,6 +3,6 @@ library(plumber)
 port <- Sys.getenv("PORT")
 if (port == "") port <- "8000"
 
-root <- pr("plumber.r")
+root <- pr("plumber.R")
 
 pr_run(root, host = "0.0.0.0", port = as.numeric(port))
