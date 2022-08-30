4ca2d6aaffa63b1591571a5055838d81148b7ebf
willemsleegers <w.sleegers@me.com>
2022-08-30 13:45:52 +0200

    prepare for deploy test

--- app.R
+++ app.R
@@ -1,11 +1,11 @@
 library(plumber)
 
-port <- Sys.getenv('PORT')
+port <- Sys.getenv("PORT")
 
 server <- plumb("plumber.R")
 
 server$run(
-	host = '0.0.0.0',
-	port = as.numeric(port),
-	swagger=TRUE
+  host = "0.0.0.0",
+  port = as.numeric(port),
+  swagger = TRUE
 )
--- plumber.R
+++ plumber.R
@@ -12,14 +12,14 @@ function(req, res) {
 #* Echo back the input
 #* @param msg The message to echo
 #* @get /echo
-function(msg=""){
+function(msg = "") {
   list(msg = paste0("The message is: '", msg, "'"))
 }
 
 #* Plot a histogram
 #* @png
 #* @get /plot
-function(){
+function() {
   rand <- rnorm(100)
   hist(rand)
 }
@@ -28,6 +28,6 @@ function(){
 #* @param a The first number to add
 #* @param b The second number to add
 #* @post /sum
-function(a, b){
+function(a, b) {
   as.numeric(a) + as.numeric(b)
 }
