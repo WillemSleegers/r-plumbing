287672bd1959054f1da782e7695f63d11bd4a740
Willem Sleegers <w.sleegers@me.com>
2022-09-12 10:29:59 +0200

    Formatting

--- app-local.r
+++ app-local.r
@@ -2,4 +2,9 @@ library(plumber)
 
 root <- pr("plumber-local.r")
 
-pr_run(root, host = "127.0.0.1", port = 8000, docs = "swagger")
+pr_run(
+  pr = root,
+  host = "127.0.0.1",
+  port = 8000,
+  docs = "swagger"
+)
--- app.r
+++ app.r
@@ -4,4 +4,9 @@ port <- Sys.getenv("PORT")
 
 root <- pr("plumber.r")
 
-pr_run(root, host = "0.0.0.0", port = as.numeric(port), docs = "swagger")
+pr_run(
+  pr = root,
+  host = "0.0.0.0",
+  port = as.numeric(port),
+  docs = "swagger"
+)
